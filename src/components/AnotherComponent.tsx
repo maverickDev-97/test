197e27d87e5ee12f8abb2f0b73c03571a69c3e11
maverickDev-97 <dmitriy.pryshliak@gmail.com>
2021-10-07 11:56:09 +0300

    Feature is ready

--- src/components/AnotherComponent.tsx
+++ src/components/AnotherComponent.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const AnotherComponent = () => {
+
+    function Feature123() {
+        console.log('Feature works');
+    }
+
     return (
         <div>
             Another Component
